dfad82fa8a9d0c27b55b969938c91c6d57a787d8
Ragavendra Nagraj <ragavendra.bn@gmail.com>
2015-12-24 22:19:57 -0800

    updated home to products page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   mount Shoppe::Engine => "/shoppe"
+  
+  get "product/:permalink", to: "products#show", as: "product"
+  post "product/:permalink", to: "products#buy", as: "buy"
+  root to: "products#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
